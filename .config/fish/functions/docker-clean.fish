f706ed465f2a8e0dd5b1f0e5c8ab78bc3b926218
Rahul AG <r@hul.ag>
2013-07-19 02:16:00 +0800

    Update docker-clean to clean images too.

--- .config/fish/functions/docker-clean.fish
+++ .config/fish/functions/docker-clean.fish
@@ -1,5 +1,17 @@
 function docker-clean
-	docker ps -q -a | sort > /tmp/dc-all
-	docker ps -q | sort > /tmp/dc-run
-	comm -2 -3 /tmp/dc-all /tmp/dc-run | xargs docker rm
+	docker ps -q -a | sort > /tmp/dcp-all
+	docker ps -q | sort > /tmp/dcp-run
+	comm -2 -3 /tmp/dcp-all /tmp/dcp-run > /tmp/dcp-dead
+	set dcp_len (cat /tmp/dcp-dead | wc -l)
+	if [ $dcp_len -gt 0 ];
+		cat /tmp/dcp-dead | xargs -L 1 docker rm
+	end
+	docker images -q -a | sort > /tmp/dci-all
+	docker images -q | sort > /tmp/dci-run
+	comm -2 -3 /tmp/dci-all /tmp/dci-run > /tmp/dci-dead
+	set dci_len (cat /tmp/dci-dead | wc -l)
+	if [ $dci_len -gt 0 ];
+		cat /tmp/dci-dead | xargs -L 1 docker rmi
+	end
+	rm -f /tmp/dc{p,i}-{all,run,dead}
 end
