d550dc50a0e550e7138ce0dc51358be897cb8429
Rahul AG <r@hul.ag>
2013-12-18 17:24:45 +0800

    Remove encrypted store rubbish.

--- install.sh
+++ install.sh
@@ -9,10 +9,3 @@ for file in $(ls -A . | egrep -v '(.git$|install.sh)'); do
 	rm -rf ~/"${file}" 2> /dev/null
 	cp -rv "$PWD/${file}" ~/"${file}"
 done
-
-read -s -p "AES Passphrase: " aes_pass
-for file in $(find . -name '*.enc' 2> /dev/null); do
-	pre_strip=${file#./}
-	echo $aes_pass | openssl enc -d -aes-256-cbc -in ~/"${pre_strip}" -out ~/"${pre_strip%.enc}" -pass stdin
-	rm -f ~/"${file}"
-done
