e32bb241b705e255fd29b33089f1be10f44cb5f2
Rahul AG <r@hul.ag>
2014-04-08 14:25:45 +0800

    Add DefaultKeyBinding.dict for OS X.

--- install.sh
+++ install.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-if [ "$(dirname $0)" != '.' ]; then
+if [[ $(dirname $0) != '.' ]]; then
 	echo "Run this from the directory it's in."
 	exit 1
 fi
 
-for file in $(ls -A . | egrep -v '(.git$|install.sh)'); do
+echo "Copying…"
+for file in $(ls -A . | grep -Ev '(.git$|install.sh)'); do
 	rm -rf ~/"${file}" 2> /dev/null
 	cp -rv "$PWD/${file}" ~/"${file}"
 done
+
+echo "Linking…"
+if [[ $(uname -s) == "Darwin" ]]; then
+	ln -vfs ~/.DefaultKeyBinding.dict ~/Library/KeyBindings/DefaultKeyBinding.dict
+fi
