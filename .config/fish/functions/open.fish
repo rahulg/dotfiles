edfa3b87874b8e3b9105cdc4d9895d75af6d7765
Rahul AG <r@hul.ag>
2014-09-10 18:34:51 +0800

    fish: Separate rtus and OS checks for open func.

--- .config/fish/functions/open.fish
+++ .config/fish/functions/open.fish
@@ -2,6 +2,8 @@ function open --description 'Open with fancy stuff'
 	set rtus reattach-to-user-namespace
 	if [ ! -x "(which $rtus)" ]
 		set rtus command
+	end
+	if [ (uname) = "Darwin" ]
 		set open open
 	else
 		set open xdg-open
