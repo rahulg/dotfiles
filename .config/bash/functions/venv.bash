1a86e3992ac1bd7a40152c5f738d66b715f4bbac
Rahul AG <r@hul.ag>
2014-10-08 18:48:20 +0800

    bash: syntax fix in venv.

--- .config/bash/functions/venv.bash
+++ .config/bash/functions/venv.bash
@@ -1,5 +1,5 @@
 venv() {
-	ver=${1:default}
+	ver=${1:-default}
 	case ${ver} in
 	2)
 		vpython=python2
@@ -13,7 +13,7 @@ venv() {
 		vpython=pypy
 		virt=venvpy
 		;;
-	*)
+	default)
 		virt=$(ls | grep -oE 'venv(2|3|py)' 2>/dev/null | head -n1)
 		if [[ -z ${virt} ]]; then
 			echo 'no venv found, and no version given'
