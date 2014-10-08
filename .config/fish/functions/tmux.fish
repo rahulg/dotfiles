3bb4a7a552a968da2c906c1ab9dcb39457661590
Rahul AG <r@hul.ag>
2014-10-08 17:05:56 +0800

    tmux: restructure config files & move to .config.

---
+++ .config/fish/functions/tmux.fish
@@ -0,0 +1,13 @@
+function tmux -d 'tmux config selection wrapper'
+
+	set tmconf tmux.conf
+	if [ (uname -s) = 'Darwin' ]
+		set tmconf osx.conf
+	else if [ (uname -s) = 'Linux' ] 
+		if set -q DISPLAY
+			set tmconf x11.conf
+		end
+	end
+
+	command tmux -f ~/.config/tmux{$tmconf} $argv
+end
