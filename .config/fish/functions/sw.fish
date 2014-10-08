421d8f1b5b74717ce179707acaa27c741e353251
Rahul AG <r@hul.ag>
2014-10-08 17:27:22 +0800

    Remove useless switch-ssh script.

--- .config/fish/functions/sw.fish
+++ .config/fish/functions/sw.fish
@@ -2,5 +2,5 @@ function sw --description 'Connects to an OFS switch'
 	set -l arg (echo $argv | sgrep -o '[0-9a-zA-Z]' | tr 'A-Z' 'a-z' | tr -d '\n')
 	set -lx num (echo $argv | sgrep -oE '^[0-9]+')
 	set -lx blk (echo $argv | sed -E 's|^[0-9]+([^0-9]+).*$|\1|')
-	command tmux -f ~/config/tmux/sw.conf new-session -s "sw$num-pat$blk" "switch-ssh sw$num.pat$blk.ofs.edu.sg"
+	command tmux -f ~/config/tmux/sw.conf new-session -s "sw$num-pat$blk" "ssh sw$num.pat$blk.ofs.edu.sg"
 end
