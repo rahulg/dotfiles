467c2bc6cef32476b8909e4923012f8ceae6976d
Rahul AG <r@hul.ag>
2014-10-14 18:12:06 +0800

    fish: fix session name for sw.fish.

--- .config/fish/functions/sw.fish
+++ .config/fish/functions/sw.fish
@@ -2,5 +2,5 @@ function sw -d 'Connects to an OFS switch'
 	set -l arg (echo $argv | sgrep -o '[0-9a-zA-Z]' | tr 'A-Z' 'a-z' | tr -d '\n')
 	set -lx num (echo $arg | sgrep -oE '^[0-9]+')
 	set -lx blk (echo $arg | sed -E 's|^[0-9]+([^0-9]+).*$|\1|')
-	command tmux -f ~/config/tmux/sw.conf new-session -s "sw{$num}.pat{$blk}" "ssh sw{$num}.pat{$blk}.ofs.edu.sg"
+	command tmux -f ~/config/tmux/sw.conf new-session -s "sw"{$num}"-pat"{$blk} "ssh sw{$num}.pat{$blk}.ofs.edu.sg"
 end
